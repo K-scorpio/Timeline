e44c2075bba5e8e0daf0828d6f9dadc36bc16338
K-scorpio <kevin.s.hartley@gmail.com>
2016-06-09 11:16:26 -0600

    finished cloud kit managed object context

---
+++ Timeline/Timeline/CloudKitManagedObject.swift
@@ -0,0 +1,9 @@
+//
+//  CloudKitManagedObject.swift
+//  Timeline
+//
+//  Created by Kevin Hartley on 6/9/16.
+//  Copyright © 2016 Hartley Development. All rights reserved.
+//
+
+import Foundation
