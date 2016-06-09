9d8ae02dfca7656e30aeb620a4c1934e1a02ad91
K-scorpio <kevin.s.hartley@gmail.com>
2016-06-09 11:16:43 -0600

    finished Cloud Kit MOC

--- Timeline/Post.swift
+++ Timeline/Post.swift
@@ -8,21 +8,23 @@
 
 import UIKit
 import CoreData
+import CloudKit
 
 
-class Post: NSManagedObject {
+class Post: NSManagedObject, CloudKitManagedObject {
     
     private let timestampKey = "timestamp"
     private let photoDataKey = "photoData"
     
-    convenience init?(photo: NSData, timestamp: NSDate, caption: String, context: NSManagedObjectContext = Stack.sharedStack.managedObjectContext) {
+    convenience init(photo: NSData, timestamp: NSDate, caption: String, context: NSManagedObjectContext = Stack.sharedStack.managedObjectContext) {
         guard let entity = NSEntityDescription.entityForName("Post", inManagedObjectContext: context) else {
-            return nil
+            fatalError("Error: Core Data failed to create entity from entity description")
         }
         self.init(entity: entity, insertIntoManagedObjectContext: context)
         
         self.photoData = photo
         self.timestamp = timestamp
+        self.recordName = self.managedObjectContext
     }
     
     var photo: UIImage? {
@@ -31,4 +33,6 @@ class Post: NSManagedObject {
         
         return UIImage(data: photoData)
     }
+    
+    
 }
--- Timeline/Timeline/CloudKitManagedObject.swift
+++ Timeline/Timeline/CloudKitManagedObject.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+import CoreData
+import CloudKit
+
+@objc protocol CloudKitManagedObject {
+    
+    var timestamp: NSDate { get set }
+    var recordIDData: NSData? { get set }
+    var recordName: String { get set }
+    var recordType: String { get set }
+    var cloudKitRecord: CKRecord? { get }
+    
+}
+
+extension CloudKitManagedObject {
+    var isSynced: Bool {
+        return recordIDData != nil
+    }
+    
+    var cloudKitRecordID: CKRecordID? {
+        guard let recordIDData = recordIDData,
+            let recordID = NSKeyedUnarchiver.unarchiveObjectWithData(recordIDData) as? CKRecordID else {
+                return nil
+        }
+        return recordID
+    }
+    
+    func update(record: CKRecord) {
+        self.recordIDData = NSKeyedArchiver.archivedDataWithRootObject(record.recordID)
+        
+        do {
+            try Stack.sharedStack.managedObjectContext.save()
+        } catch {
+            print("Unable to save MOC: \(error)")
+        }
+    }
+    
+    var cloudKitReference: CKReference? {
+        guard let recordID = cloudKitRecordID else { return nil }
+        return CKReference(recordID: recordID, action: .None)
+    }
+    
+    func nameForManagedObject() -> String {
+        return NSUUID().UUIDString
+    }
+}
